758aa90ceae53bca9a1f82d99fe9a7472aa33dd6
kperin <kristopher.perin@gmail.com>
2018-01-07 19:47:04 +0100

    first commit

--- src/BanqueBundle/Controller/BanqueController.php
+++ src/BanqueBundle/Controller/BanqueController.php
@@ -6,7 +6,8 @@ use App\BanqueBundle\Entity\Banque;
 use App\BanqueBundle\Form\BanqueType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Banque controller.
--- src/UserBundle/UserBundle.php
+++ src/UserBundle/UserBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Core\UserBundle;
+namespace UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
